861459781074d8b9f20d11ba095114a3b9ad3659
Divyansh Saxena <thedivyanshsaxena@gmail.com>
2026-09-15 09:33:41 +0530

    corrected

--- lab4/app.js
+++ lab4/app.js
@@ -1,5 +1,4 @@
 import http from "http";
-// import * as teams from "teams.js";
 import { getAllTeams, addTeam, getTeamById } from "./teams.js";
 import { parse as parseUrl } from "url";
 
@@ -35,22 +34,42 @@ const server = http.createServer(async (req, res) => {
   console.log("Method:", method);
 
   if (pathname === "/api/v1/teams" && method === "GET") {
+
     let teams = getAllTeams();
     return sendJson(res, 200, teams, "count", teams.length);
-  } else if (pathname === "/api/v1/teams" && method == "POST") {
+
+  } 
+  
+  else if (pathname === "/api/v1/teams" && method == "POST") {
+
     const { tname, tl, members } = await parseJSONBody(req);
     if (!tname || !tl || !members)
-      return sendJson(400, {
+      return sendJson(res, 400, {
         error: "Team Name, Team Leader, or Members not defined",
       });
     const team = addTeam({ tname, tl, members });
 
     return sendJson(res, 201, team, "Message", "Team registered successfully");
+
+  } 
+  
+  
+  
+  else if (pathname.startsWith("/api/v1/teams/") && method === "GET") {
+    const id = Number(pathname.split("/").pop());
+    const team = getTeamById(id);
+
+    if (!team)
+      return sendJson(res, 400, {
+        error: `Team with id: ${id} not found`,
+      });
+    return sendJson(res, 200, team, "Message", "Team Found");
   } else {
     res.statusCode = 404;
-    res.end();
+    res.end("Not matching");
   }
 });
+
 server.listen(PORT, () => {
-  console.log(" SIH Server Is Running at ",PORT);
+  console.log("SIH Server is running at ", PORT);
 });
