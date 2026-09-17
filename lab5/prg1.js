fa581572ae5d1c8ef9a03ccc25b348210b0a2971
Divyansh Saxena <thedivyanshsaxena@gmail.com>
2026-09-17 14:42:45 +0530

    sending html file to the client

---
+++ lab5/prg1.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import path from'path';
+import { fileURLToPath } from "node:url";
+
+const port=3333
+const app=express()
+
+const filename=fileURLToPath(import.meta.url);
+const dirname=path.dirname(filename);
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(dirname,"Public","index.html"))
+})
+
+app.get("/about",(req,res)=>{
+    res.sendFile(path.join(dirname,"Public","about.html"))
+})
+
+app.get("/enquiry",(req,res)=>{
+    res.sendFile(path.join(dirname,"Public","enquiry.html"))
+})
+
+app.listen(port,()=> console.log("prg1 is running at ",port));
