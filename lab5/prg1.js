8d4dde42539a31a09dd62c3015d1bd1fa3e1e725
Divyansh Saxena <thedivyanshsaxena@gmail.com>
2026-09-17 20:57:57 +0530

    sending html file to the client

--- lab5/prg1.js
+++ lab5/prg1.js
@@ -1,23 +1,23 @@
-import express from 'express';
-import path from'path';
+import express from "express";
+import path from "path";
 import { fileURLToPath } from "node:url";
 
-const port=3333
-const app=express()
+const port = 3333;
+const app = express();
 
-const filename=fileURLToPath(import.meta.url);
-const dirname=path.dirname(filename);
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(dirname,"Public","index.html"))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(dirname, "Public", "index.html"));
+});
 
-app.get("/about",(req,res)=>{
-    res.sendFile(path.join(dirname,"Public","about.html"))
-})
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(dirname, "Public", "about.html"));
+});
 
-app.get("/enquiry",(req,res)=>{
-    res.sendFile(path.join(dirname,"Public","enquiry.html"))
-})
+app.get("/enquiry", (req, res) => {
+  res.sendFile(path.join(dirname, "Public", "enquiry.html"));
+});
 
-app.listen(port,()=> console.log("prg1 is running at ",port));
+app.listen(port, () => console.log("prg1 is running at ", port));
